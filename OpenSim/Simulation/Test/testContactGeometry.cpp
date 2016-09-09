b3b2619ae257502838b9e8028d5b6ff85259fe4e
Tom Uchida <tom.uchida@gmail.com>
2016-09-09 01:54:57 -0700

    Untabify.

--- OpenSim/Simulation/Test/testContactGeometry.cpp
+++ OpenSim/Simulation/Test/testContactGeometry.cpp
@@ -328,17 +328,17 @@ try {
     // Simulate it and see if it bounces correctly.
     cout << "stateY=" << osim_state.getY() << std::endl;
 
-	std::cout << "  create integrator" << std::endl;
+    std::cout << "  create integrator" << std::endl;
     RungeKuttaMersonIntegrator integrator(osimModel.getMultibodySystem() );
-	std::cout << "  set accuracy" << std::endl;
+    std::cout << "  set accuracy" << std::endl;
     integrator.setAccuracy(integ_accuracy);
-	std::cout << "  set maximum step size" << std::endl;
+    std::cout << "  set maximum step size" << std::endl;
     integrator.setMaximumStepSize(100*integ_accuracy);
-	std::cout << "  create manager" << std::endl;
+    std::cout << "  create manager" << std::endl;
     Manager manager(osimModel, integrator);
-	std::cout << "  set initial time" << std::endl;
+    std::cout << "  set initial time" << std::endl;
     manager.setInitialTime(0.0);
-	std::cout << "  set final time" << std::endl;
+    std::cout << "  set final time" << std::endl;
     manager.setFinalTime(duration);
     std::cout << "  integrating...";
     manager.integrate(osim_state);
