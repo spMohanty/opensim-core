59b689e8d2691f6ec76847e37fafd0866337bbc5
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-11-26 10:55:17 -0800

    More human readable script for preparing the distribution

--- dist/prepare_dist.sh
+++ dist/prepare_dist.sh
@@ -1,28 +1,45 @@
-rm amd64 -r
-mkdir amd64
-mkdir amd64/opensim_4.0-1kidzik
-mkdir amd64/opensim_4.0-1kidzik/usr
-mkdir amd64/opensim_4.0-1kidzik/usr/bin
-cp ~/opensim_dependencies_install/simbody/libexec/simbody/simbody-visualizer amd64/opensim_4.0-1kidzik/usr/bin/
-cp ~/opensim_install/lib amd64/opensim_4.0-1kidzik/usr/lib -r
-cp ~/opensim_dependencies_install/BTK/lib/*/* amd64/opensim_4.0-1kidzik/usr/lib/
-rm amd64/opensim_4.0-1kidzik/usr/lib/cmake -r
-rm amd64/opensim_4.0-1kidzik/usr/lib/python3.5/site-packages/*.py*
-rm amd64/opensim_4.0-1kidzik/usr/lib/python3.5/site-packages/opensim/__pycache__ -r
-rm amd64/opensim_4.0-1kidzik/usr/lib/python3.5/site-packages/opensim/tests -r
-mv amd64/opensim_4.0-1kidzik/usr/lib/python3.5/site-packages amd64/opensim_4.0-1kidzik/usr/lib/python3.5/dist-packages
-mv amd64/opensim_4.0-1kidzik/usr/lib/python3.5 amd64/opensim_4.0-1kidzik/usr/lib/python3
-mkdir amd64/opensim_4.0-1kidzik/DEBIAN
-echo "Package: opensim
+# Settings
+PKGNAME=python3-opensim
+PKGVER=4.0-1kidzik
+PKGARCH=amd64
+
+# Prepare path
+PREFIX=$PKGARCH/$PKGNAME-$PKGVER
+
+# Prepare the directories
+rm $PKGARCH-r
+mkdir $PREFIX -p
+mkdir $PREFIX/usr/bin -p
+
+# Copy the build
+cp ~/opensim_dependencies_install/simbody/libexec/simbody/simbody-visualizer $PREFIX/usr/bin/
+cp ~/opensim_install/lib $PREFIX/usr/lib -r
+cp ~/opensim_dependencies_install/BTK/lib/*/* $PREFIX/usr/lib/
+
+# Remove not used files
+rm $PREFIX/usr/lib/cmake -r
+rm $PREFIX/usr/lib/python3.5/site-packages/*.py*
+rm $PREFIX/usr/lib/python3.5/site-packages/opensim/__pycache__ -r
+rm $PREFIX/usr/lib/python3.5/site-packages/opensim/tests -r
+
+# Make it python3 compatible
+mv $PREFIX/usr/lib/python3.5/site-packages $PREFIX/usr/lib/python3.5/dist-packages
+mv $PREFIX/usr/lib/python3.5 $PREFIX/usr/lib/python3
+
+# Debian description
+mkdir $PREFIX/DEBIAN
+echo "Package: python3-opensim
 Version: 4.0-1kidzik
 Section: base
 Priority: optional
 Architecture: amd64
-Depends: freeglut3, freeglut3-dev
+Depends: freeglut3, freeglut3-dev, liblapack-dev
 Maintainer: Lukasz Kidzinski <lukasz.kidzinski@stanford.edu>
 Description: An UNOFFICIAL build of OpenSim 4.0. 
  This package is meant to use with the reinforcement
  learning osim-rl python environment only. For any
  other usage please refer to
- https://github.com/opensim-org/opensim-core" > amd64/opensim_4.0-1kidzik/DEBIAN/control
-dpkg-deb --build amd64/opensim_4.0-1kidzik
+ https://github.com/opensim-org/opensim-core" > $PREFIX/DEBIAN/control
+
+# Create the package
+dpkg-deb --build $PREFIX
