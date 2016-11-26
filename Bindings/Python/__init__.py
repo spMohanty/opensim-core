3ac640d77dd45a6fa247339fd7e3bbb1c165f100
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2016-11-26 11:28:18 -0800

    Typo

--- Bindings/Python/__init__.py
+++ Bindings/Python/__init__.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 from simbody import *
 from common import *
 from simulation import *
